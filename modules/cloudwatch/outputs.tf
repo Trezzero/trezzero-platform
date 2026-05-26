9c13ceda8f155b8612959b687b822a2b8cc9886a
Shaun Trezero <shaun@trezzero.co.za>
2026-05-26 09:17:14 +0000

    terraform fmt for cloudwatch

--- modules/cloudwatch/outputs.tf
+++ modules/cloudwatch/outputs.tf
@@ -1,3 +1,11 @@
-output "log_group_backend" { value = aws_cloudwatch_log_group.backend.name }
-output "log_group_prometheus" { value = aws_cloudwatch_log_group.prometheus.name }
-output "log_group_grafana" { value = aws_cloudwatch_log_group.grafana.name }
+output "log_group_backend" {
+  value = aws_cloudwatch_log_group.backend.name
+}
+
+output "log_group_prometheus" {
+  value = aws_cloudwatch_log_group.prometheus.name
+}
+
+output "log_group_grafana" {
+  value = aws_cloudwatch_log_group.grafana.name
+}
--- modules/cloudwatch/variables.tf
+++ modules/cloudwatch/variables.tf
@@ -1,3 +1,16 @@
-variable "project_name" { type = string }
-variable "environment" { type = string }
-variable "log_retention_days" { type = number default = 7 }
+variable "project_name" {
+  description = "Name of the whole project"
+  type        = string
+  default     = "trezzero"
+}
+
+variable "environment" {
+  description = "App Environment"
+  type        = string
+}
+
+variable "log_retention_days" {
+  description = "Number of days logs are retained for before being deleted"
+  type    = number
+  default = 5
+}
